3542040d5bd2f177f56949ef79509789eb668af2
Dave Hubertus <davehubertus@gmail.com>
2017-07-27 08:34:45 -0600

    added test file and required packages

---
+++ test/routes.spec.js
@@ -0,0 +1,8 @@
+// process.env.NODE_ENV = "test";
+const chai = require("chai");
+const should = chai.should();
+const chaiHttp = require("chai-http");
+const server = require("../server.js");
+const knex = require("../db/knex.js");
+
+chai.use(chaiHttp);
