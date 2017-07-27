a5361921ddc6c157f3727875567cc375b26d5290
Dave Hubertus <davehubertus@gmail.com>
2017-07-27 08:29:09 -0600

    added knex file

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require(path.join(__dirname, './knexfile.js'))[environment];
+const database = require('knex')(configuration);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
